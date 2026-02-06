232e5349a7d3338307177d89758dcf29dd008ed4
bsew <ethan.wernich@baruchstudio.co.za>
2026-02-06 15:18:07 +0200

    Fixed purchase service

--- app.config.js
+++ app.config.js
@@ -33,9 +33,7 @@ export default {
       favicon: './assets/favicon.png',
       bundler: 'metro',
     },
-    plugins: ['expo-router',
-      "react-native-purchases"
-    ],
+    plugins: ['expo-router', 'react-native-purchases'],
     experiments: {
       typedRoutes: true,
     },
