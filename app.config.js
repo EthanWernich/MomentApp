ab07361d56b7f4c161864448b2189d862eb212ac
bsew <ethan.wernich@baruchstudio.co.za>
2026-02-06 15:51:02 +0200

    Fixed hopefully

--- app.config.js
+++ app.config.js
@@ -13,14 +13,17 @@ export default {
       backgroundColor: '#020202',
     },
     assetBundlePatterns: ['**/*'],
+
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.ethanwernich.moment',
+
+
       infoPlist: {
-        UIBackgroundModes: ['remote-notification'],
         ITSAppUsesNonExemptEncryption: false,
       },
     },
+
     android: {
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
@@ -29,14 +32,18 @@ export default {
       package: 'com.ethanwernich.moment',
       permissions: [],
     },
+
     web: {
       favicon: './assets/favicon.png',
       bundler: 'metro',
     },
+
     plugins: ['expo-router'],
+
     experiments: {
       typedRoutes: true,
     },
+
     extra: {
       eas: {
         projectId: '3f914cee-9655-4bd2-8d47-43b5f9b17c85',
